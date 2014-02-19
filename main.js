17ec3b37ea9310dc551b9b16af66c80be7b01ae3
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-02-19 16:21:01 +0000

    file menu

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ window.onload = function() {
 
   $("#examples").change(function(ev) {
       var file = this.options[this.selectedIndex].value;
-      $.get("http://crguezl.github.io/ull-etsii-grado-pl-minijavascript/"+file, function (data) {
+      $.get("/ull-etsii-grado-pl-minijavascript/"+file, function (data) {
               $("#INPUT").val(data);
           });
   });
